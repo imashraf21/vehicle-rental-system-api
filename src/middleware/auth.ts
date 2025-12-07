94333fe0de0968973b32d41baef89e12e4d2359a
imashraf21 <imashraf48@gmail.com>
2025-12-07 23:24:19 +0600

    auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+
+const auth = (...roles: string[]) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const token = req.headers.authorization;
+
+			if (!token) {
+				return res.status(500).json({ message: "You're not allowed" });
+			}
+
+			const decoded = jwt.verify(
+				token,
+				config.jwtSecret as string
+			) as JwtPayload;
+
+			if (roles.length && !roles.includes(decoded.role as string)) {
+				return res.status(403).json({
+					message: "Unauthorized",
+				});
+			}
+		} catch (error: any) {
+			res.status(500).json({
+				success: false,
+				message: error.message,
+			});
+		}
+	};
+};
